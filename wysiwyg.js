12e26de435ffe2e6897b874c817d3f8df3aad49d
Chandler Morrison <cwm795@gmail.com>
2017-07-11 14:32:26 -0500

    added borders with dotted lines that de-select once you click on another card

--- wysiwyg.js
+++ wysiwyg.js
@@ -46,22 +46,46 @@ myRequest.send();
 
 function activateClickEvents() {
     for (var i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", function(e){
+        cards[i].addEventListener("click", function(e) {
             activateFocusEvent();
+            deathCard()
             activateBorderEvent(e.currentTarget)
 
         });
     }
 }
+
 function activateFocusEvent() {
-input.focus();
+    input.focus();
 }
 
 
 function activateBorderEvent(clickedCard) {
     console.log("cards", clickedCard);
+    clickedCard.classList.add("selectedCard");
+    activateKeyEvent(clickedCard);
+}
 
+function deathCard() {
+    console.log("cards", cards)
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].classList.contains("selectedCard")) {
+            cards[i].classList.remove("selectedCard")
+
+
+        };
+    }
+}
+
+function activateKeyEvent(clickedCard) {
+    // console.log("activateKeyEvent", clickedCard)
+    input.addEventListener("keyup", function() {
+        mirrorText();
+    });
 }
 
 
+function mirrorText(clickedCard) {
+console.log("mirrorText", mirrorText)
 
+};

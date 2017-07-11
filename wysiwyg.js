227939ebb8d6bbabbb139e031996d48ee46d70c9
Chandler Morrison <cwm795@gmail.com>
2017-07-11 11:05:18 -0500

    added yellow/blue coloring to elements.

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,7 +1,7 @@
 console.log("Hi");
 
 let input = document.getElementById("input");
-let card = document.getElementById("");
+let cards = document.getElementsByClassName("cards");
 let container = document.getElementById("container");
 let bio = "";
 let array = [];
@@ -18,12 +18,13 @@ function outputCards(peopleArray) {
     for (i = 0; i < peopleArray.length; i++) {
 
         console.log(peopleArray[i].title)
-        container.innerHTML += `<person>
+        container.innerHTML += `<div class="cards"><person>
          <header>${peopleArray[i].name}  ${peopleArray[i].title}</header>
          <section>${peopleArray[i].bio}  <img src="${peopleArray[i].image}"> </img></section>
-         <footer>${peopleArray[i].lifespan}</footer>
-    	</person>`
+         <footer>${peopleArray[i].lifespan.birth} & ${peopleArray[i].lifespan.death}</footer>
+    	</person></div>`
     }
+    dottedBorder();
 };
 
 
@@ -42,3 +43,12 @@ myRequest.addEventListener("error", XHRFail);
 myRequest.open("GET", "items.json");
 // console.log("data", myRequest);
 myRequest.send();
+
+
+
+function dottedBorder() {
+console.log("cards",cards);
+
+
+
+}

e0f5c04f6b7b28dc19c65ec32a4c4664122a0cf3
Chandler Morrison <cwm795@gmail.com>
2017-07-11 09:41:42 -0500

    added photos and styled them to look decent

---
+++ wysiwyg.js
@@ -0,0 +1,44 @@
+console.log("Hi");
+
+let input = document.getElementById("input");
+let card = document.getElementById("");
+let container = document.getElementById("container");
+let bio = "";
+let array = [];
+
+
+function setArr() {
+    array = JSON.parse(this.responseText);
+    console.log(array)
+    outputCards(array);
+
+};
+
+function outputCards(peopleArray) {
+    for (i = 0; i < peopleArray.length; i++) {
+
+        console.log(peopleArray[i].title)
+        container.innerHTML += `<person>
+         <header>${peopleArray[i].name} & ${peopleArray[i].title}</header>
+         <section>${peopleArray[i].bio} & <img src="${peopleArray[i].image}"> </img>[i].image}section
+         <footer>${peopleArray[i].lifespan}</footer>
+    	</person>`
+    }
+};
+
+
+
+
+
+function XHRFail() {
+
+};
+
+var myRequest = new XMLHttpRequest();
+
+myRequest.addEventListener("load", setArr);
+myRequest.addEventListener("error", XHRFail);
+
+myRequest.open("GET", "items.json");
+// console.log("data", myRequest);
+myRequest.send();
